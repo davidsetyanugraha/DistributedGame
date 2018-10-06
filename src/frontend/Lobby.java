ae2bb991422cdc59dfcfd04584a7a05b399e0454
lputra24 <lputra@student.unimelb.edu au>
2018-10-07 03:28:46 +1100

    add invite player UI

--- src/frontend/Lobby.java
+++ src/frontend/Lobby.java
@@ -43,7 +43,7 @@ public class Lobby extends JFrame {
 	 */
 	public Lobby() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 724, 512);
+		setBounds(100, 100, 800, 800);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setBackground(new Color(51, 102, 153));
@@ -52,33 +52,33 @@ public class Lobby extends JFrame {
 		contentPane.setLayout(sl_contentPane);
 		
 		JButton btnCreateNewGame = new JButton("Create New Game");
+		sl_contentPane.putConstraint(SpringLayout.WEST, btnCreateNewGame, 255, SpringLayout.WEST, contentPane);
+		sl_contentPane.putConstraint(SpringLayout.SOUTH, btnCreateNewGame, -466, SpringLayout.SOUTH, contentPane);
+		sl_contentPane.putConstraint(SpringLayout.EAST, btnCreateNewGame, -244, SpringLayout.EAST, contentPane);
 		btnCreateNewGame.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				// TODO check if game exists
 			}
 		});
-		sl_contentPane.putConstraint(SpringLayout.WEST, btnCreateNewGame, 205, SpringLayout.WEST, contentPane);
-		sl_contentPane.putConstraint(SpringLayout.EAST, btnCreateNewGame, 464, SpringLayout.WEST, contentPane);
 		contentPane.add(btnCreateNewGame);
 		
 		JButton btnPlayerList = new JButton("Player List");
-		sl_contentPane.putConstraint(SpringLayout.SOUTH, btnCreateNewGame, -47, SpringLayout.NORTH, btnPlayerList);
-		sl_contentPane.putConstraint(SpringLayout.NORTH, btnPlayerList, 228, SpringLayout.NORTH, contentPane);
+		sl_contentPane.putConstraint(SpringLayout.NORTH, btnPlayerList, 76, SpringLayout.SOUTH, btnCreateNewGame);
+		sl_contentPane.putConstraint(SpringLayout.WEST, btnPlayerList, 275, SpringLayout.WEST, contentPane);
+		sl_contentPane.putConstraint(SpringLayout.SOUTH, btnPlayerList, -330, SpringLayout.SOUTH, contentPane);
+		sl_contentPane.putConstraint(SpringLayout.EAST, btnPlayerList, -254, SpringLayout.EAST, contentPane);
 		btnPlayerList.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				// TODO show current player in lobby
 			}
 		});
-		sl_contentPane.putConstraint(SpringLayout.WEST, btnPlayerList, 263, SpringLayout.WEST, contentPane);
-		sl_contentPane.putConstraint(SpringLayout.SOUTH, btnPlayerList, -137, SpringLayout.SOUTH, contentPane);
-		sl_contentPane.putConstraint(SpringLayout.EAST, btnPlayerList, -266, SpringLayout.EAST, contentPane);
 		contentPane.add(btnPlayerList);
 		
 		JLabel lblUsername = new JLabel("SCRABBLE ONLINE");
-		sl_contentPane.putConstraint(SpringLayout.NORTH, btnCreateNewGame, 53, SpringLayout.SOUTH, lblUsername);
+		sl_contentPane.putConstraint(SpringLayout.NORTH, btnCreateNewGame, 108, SpringLayout.SOUTH, lblUsername);
 		sl_contentPane.putConstraint(SpringLayout.NORTH, lblUsername, 49, SpringLayout.NORTH, contentPane);
-		sl_contentPane.putConstraint(SpringLayout.WEST, lblUsername, 175, SpringLayout.WEST, contentPane);
-		sl_contentPane.putConstraint(SpringLayout.EAST, lblUsername, 37, SpringLayout.EAST, btnCreateNewGame);
+		sl_contentPane.putConstraint(SpringLayout.WEST, lblUsername, 219, SpringLayout.WEST, contentPane);
+		sl_contentPane.putConstraint(SpringLayout.EAST, lblUsername, -213, SpringLayout.EAST, contentPane);
 		lblUsername.setHorizontalAlignment(SwingConstants.CENTER);
 	    lblUsername.setFont(new Font("Verdana", Font.PLAIN, 26));
 	    lblUsername.setForeground(Color.ORANGE);
