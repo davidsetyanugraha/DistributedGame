0b03074f6950e705888651c7206382a426d5bd1f
andre-lee <andrelee@windowslive.com>
2018-10-14 10:32:41 +0700

    notification when registration successful and add action listener to cancel button

--- src/frontend/Login.java
+++ src/frontend/Login.java
@@ -18,152 +18,151 @@ import backend.IRemoteGame;
 
 public class Login extends JFrame {
 
-  private JPanel contentPane;
-  private JTextField userNameArea;
-  private JPasswordField passwordArea;
+	private JPanel contentPane;
+	private JTextField userNameArea;
+	private JPasswordField passwordArea;
 
-  /**
-   * Create the frame.
-   * 
-   * @param remoteGame
-   * @param client
-   */
-  public Login(IRemoteGame remoteGame) {
-    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    setBounds(100, 100, 350, 250);
-    contentPane = new JPanel();
-    contentPane.setBackground(new Color(51, 102, 153));
-    contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-    setContentPane(contentPane);
-    contentPane.setLayout(null);
+	/**
+	 * Create the frame.
+	 * 
+	 * @param remoteGame
+	 * @param client
+	 */
+	public Login(IRemoteGame remoteGame) {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 350, 250);
+		contentPane = new JPanel();
+		contentPane.setBackground(new Color(51, 102, 153));
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
 
-    JLabel Minimise = new JLabel("-");
-    Minimise.addMouseListener(new MouseAdapter() {
-      @Override
-      public void mouseClicked(MouseEvent e) {
-        setState(JFrame.ICONIFIED);
-      }
-    });
-    Minimise.setHorizontalAlignment(SwingConstants.CENTER);
-    Minimise.setFont(new Font("Arial", Font.BOLD, 28));
-    Minimise.setForeground(Color.WHITE);
-    Minimise.setBounds(282, 3, 27, 40);
-    contentPane.add(Minimise);
+		JLabel Minimise = new JLabel("-");
+		Minimise.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				setState(JFrame.ICONIFIED);
+			}
+		});
+		Minimise.setHorizontalAlignment(SwingConstants.CENTER);
+		Minimise.setFont(new Font("Arial", Font.BOLD, 28));
+		Minimise.setForeground(Color.WHITE);
+		Minimise.setBounds(282, 3, 27, 40);
+		contentPane.add(Minimise);
 
-    JLabel Exit = new JLabel("X");
-    Exit.addMouseListener(new MouseAdapter() {
-      @Override
-      public void mouseClicked(MouseEvent e) {
-        System.exit(0);
-      }
-    });
-    Exit.setHorizontalAlignment(SwingConstants.CENTER);
-    Exit.setFont(new Font("Arial", Font.BOLD, 17));
-    Exit.setForeground(Color.WHITE);
-    Exit.setBounds(314, 5, 36, 38);
-    contentPane.add(Exit);
+		JLabel Exit = new JLabel("X");
+		Exit.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				System.exit(0);
+			}
+		});
+		Exit.setHorizontalAlignment(SwingConstants.CENTER);
+		Exit.setFont(new Font("Arial", Font.BOLD, 17));
+		Exit.setForeground(Color.WHITE);
+		Exit.setBounds(314, 5, 36, 38);
+		contentPane.add(Exit);
 
-    JLabel Header = new JLabel("SCRABBLE ONLINE");
-    Header.setHorizontalAlignment(SwingConstants.CENTER);
-    Header.setFont(new Font("Verdana", Font.PLAIN, 26));
-    Header.setForeground(Color.ORANGE);
-    Header.setBounds(0, 42, 350, 40);
-    contentPane.add(Header);
+		JLabel Header = new JLabel("SCRABBLE ONLINE");
+		Header.setHorizontalAlignment(SwingConstants.CENTER);
+		Header.setFont(new Font("Verdana", Font.PLAIN, 26));
+		Header.setForeground(Color.ORANGE);
+		Header.setBounds(0, 42, 350, 40);
+		contentPane.add(Header);
 
-    JLabel lblUsername = new JLabel("Username ");
-    lblUsername.setForeground(Color.WHITE);
-    lblUsername.setFont(new Font("Tahoma", Font.PLAIN, 12));
-    lblUsername.setBounds(60, 102, 69, 23);
-    contentPane.add(lblUsername);
+		JLabel lblUsername = new JLabel("Username ");
+		lblUsername.setForeground(Color.WHITE);
+		lblUsername.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		lblUsername.setBounds(60, 102, 69, 23);
+		contentPane.add(lblUsername);
 
-    JLabel lblPassword = new JLabel("Password ");
-    lblPassword.setForeground(Color.WHITE);
-    lblPassword.setFont(new Font("Tahoma", Font.PLAIN, 12));
-    lblPassword.setBounds(60, 137, 69, 23);
-    contentPane.add(lblPassword);
+		JLabel lblPassword = new JLabel("Password ");
+		lblPassword.setForeground(Color.WHITE);
+		lblPassword.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		lblPassword.setBounds(60, 137, 69, 23);
+		contentPane.add(lblPassword);
 
-    userNameArea = new JTextField();
-    userNameArea.setForeground(new Color(255, 255, 255));
-    userNameArea.setFont(new Font("Tahoma", Font.PLAIN, 13));
-    userNameArea.setBackground(new Color(102, 102, 102));
-    userNameArea.setBounds(129, 103, 168, 20);
-    contentPane.add(userNameArea);
-    userNameArea.setColumns(10);
+		userNameArea = new JTextField();
+		userNameArea.setForeground(new Color(255, 255, 255));
+		userNameArea.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		userNameArea.setBackground(new Color(102, 102, 102));
+		userNameArea.setBounds(129, 103, 168, 20);
+		contentPane.add(userNameArea);
+		userNameArea.setColumns(10);
 
-    passwordArea = new JPasswordField();
-    passwordArea.setForeground(new Color(255, 255, 255));
-    passwordArea.setFont(new Font("Tahoma", Font.PLAIN, 13));
-    passwordArea.setBackground(new Color(102, 102, 102));
-    passwordArea.setBounds(129, 138, 168, 20);
-    contentPane.add(passwordArea);
+		passwordArea = new JPasswordField();
+		passwordArea.setForeground(new Color(255, 255, 255));
+		passwordArea.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		passwordArea.setBackground(new Color(102, 102, 102));
+		passwordArea.setBounds(129, 138, 168, 20);
+		contentPane.add(passwordArea);
 
-    JButton loginBtn = new JButton("Login");
-    loginBtn.addMouseListener(new MouseAdapter() {
-      @Override
-      public void mouseClicked(MouseEvent e) {
-        // when button login is pressed
-        char[] pass = passwordArea.getPassword();
-        String passString = new String(pass);
-        try {
-          // when button login is pressed, check the username and password
-          // client can join the remoteGame if the validation is okay.
+		JButton loginBtn = new JButton("Login");
+		loginBtn.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				// when button login is pressed
+				char[] pass = passwordArea.getPassword();
+				String passString = new String(pass);
+				try {
+					// when button login is pressed, check the username and password
+					// client can join the remoteGame if the validation is okay.
 
-          if (remoteGame.isLoginValid(userNameArea.getText(), passString)) {
+					if (remoteGame.isLoginValid(userNameArea.getText(), passString)) {
 
-            if (!remoteGame.isClientLoggedIn(userNameArea.getText())) {
-              Client client = new Client(userNameArea.getText());
-              client.joinClientList(remoteGame);
-              // dispose login frame
-              dispose();
+						if (!remoteGame.isClientLoggedIn(userNameArea.getText())) {
+							Client client = new Client(userNameArea.getText());
+							client.joinClientList(remoteGame);
+							// dispose login frame
+							dispose();
 
-              Lobby lobby = new Lobby(client, remoteGame);
-              lobby.setVisible(true);
-            } else {
-              JOptionPane.showMessageDialog(null,
-                  "Username has been used. Please try another username", "Error",
-                  JOptionPane.PLAIN_MESSAGE);
-            }
-          } else {
-            JOptionPane.showMessageDialog(null,
-                "Username or password not valid. Please retype again", "Error",
-                JOptionPane.PLAIN_MESSAGE);
-          }
+							Lobby lobby = new Lobby(client, remoteGame);
+							lobby.setVisible(true);
+						} else {
+							JOptionPane.showMessageDialog(null, "Username has been used. Please try another username",
+									"Error", JOptionPane.PLAIN_MESSAGE);
+						}
+					} else {
+						JOptionPane.showMessageDialog(null, "Username or password not valid. Please retype again",
+								"Error", JOptionPane.PLAIN_MESSAGE);
+					}
 
-        } catch (RemoteException e1) {
-          e1.printStackTrace();
-        }
-      }
-    });
-    loginBtn.setBackground(new Color(255, 255, 255));
-    loginBtn.setFont(new Font("Tahoma", Font.PLAIN, 13));
-    loginBtn.setBounds(70, 172, 90, 29);
-    contentPane.add(loginBtn);
+				} catch (RemoteException e1) {
+					e1.printStackTrace();
+				}
+			}
+		});
+		loginBtn.setBackground(new Color(255, 255, 255));
+		loginBtn.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		loginBtn.setBounds(70, 172, 90, 29);
+		contentPane.add(loginBtn);
 
-    JButton cancelBtn = new JButton("Cancel");
-    cancelBtn.addMouseListener(new MouseAdapter() {
-      @Override
-      public void mouseClicked(MouseEvent e) {
-        System.exit(0);
-      }
-    });
-    cancelBtn.setFont(new Font("Tahoma", Font.PLAIN, 13));
-    cancelBtn.setBounds(179, 172, 90, 29);
-    contentPane.add(cancelBtn);
+		JButton cancelBtn = new JButton("Cancel");
+		cancelBtn.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				System.exit(0);
+			}
+		});
+		cancelBtn.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		cancelBtn.setBounds(179, 172, 90, 29);
+		contentPane.add(cancelBtn);
 
-    JLabel register = new JLabel("Click here to create a new account");
-    register.addMouseListener(new MouseAdapter() {
-      @Override
-      public void mouseClicked(MouseEvent e) {
-        Register regis = new Register(remoteGame);
-        regis.setVisible(true);
-        regis.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      }
-    });
-    register.setFont(new Font("Tahoma", Font.PLAIN, 12));
-    register.setForeground(new Color(255, 255, 255));
-    register.setHorizontalAlignment(SwingConstants.CENTER);
-    register.setBounds(80, 213, 195, 16);
-    contentPane.add(register);
-    setUndecorated(true);
-  }
+		JLabel register = new JLabel("Click here to create a new account");
+		register.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				dispose();
+				Register regis = new Register(remoteGame);
+				regis.setVisible(true);
+				regis.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			}
+		});
+		register.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		register.setForeground(new Color(255, 255, 255));
+		register.setHorizontalAlignment(SwingConstants.CENTER);
+		register.setBounds(80, 213, 195, 16);
+		contentPane.add(register);
+		setUndecorated(true);
+	}
 }
--- src/frontend/Register.java
+++ src/frontend/Register.java
@@ -18,188 +18,202 @@ import backend.IRemoteGame;
 
 public class Register extends JFrame {
 
-  private JPanel regPane;
-  private JTextField firstNameArea;
-  private JTextField lastNameArea;
-  private JTextField userNameArea;
-  private JPasswordField passwordArea;
-  private JPasswordField confirmationArea;
+	private JPanel regPane;
+	private JTextField firstNameArea;
+	private JTextField lastNameArea;
+	private JTextField userNameArea;
+	private JPasswordField passwordArea;
+	private JPasswordField confirmationArea;
 
-  /**
-   * Create the frame.
-   */
-  public Register(IRemoteGame remoteGame) {
-    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    setBounds(100, 100, 340, 260);
-    regPane = new JPanel();
-    regPane.setBackground(new Color(51, 102, 153));
-    regPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-    setContentPane(regPane);
-    regPane.setLayout(null);
+	/**
+	 * Create the frame.
+	 */
+	public Register(IRemoteGame remoteGame) {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 340, 260);
+		regPane = new JPanel();
+		regPane.setBackground(new Color(51, 102, 153));
+		regPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(regPane);
+		regPane.setLayout(null);
 
-    JLabel welcomeMsg = new JLabel("Welcome new player! Tell us about yourself.");
-    welcomeMsg.setHorizontalAlignment(SwingConstants.LEFT);
-    welcomeMsg.setForeground(Color.ORANGE);
-    welcomeMsg.setFont(new Font("Verdana", Font.PLAIN, 12));
-    welcomeMsg.setBounds(26, 29, 269, 33);
-    regPane.add(welcomeMsg);
+		JLabel welcomeMsg = new JLabel("Welcome new player! Tell us about yourself.");
+		welcomeMsg.setHorizontalAlignment(SwingConstants.LEFT);
+		welcomeMsg.setForeground(Color.ORANGE);
+		welcomeMsg.setFont(new Font("Verdana", Font.PLAIN, 12));
+		welcomeMsg.setBounds(26, 29, 269, 33);
+		regPane.add(welcomeMsg);
 
-    JLabel firstNameLbl = new JLabel("Last Name    ");
-    firstNameLbl.setForeground(Color.WHITE);
-    firstNameLbl.setFont(new Font("Tahoma", Font.PLAIN, 12));
-    firstNameLbl.setBounds(26, 94, 109, 23);
-    regPane.add(firstNameLbl);
+		JLabel firstNameLbl = new JLabel("Last Name    ");
+		firstNameLbl.setForeground(Color.WHITE);
+		firstNameLbl.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		firstNameLbl.setBounds(26, 94, 109, 23);
+		regPane.add(firstNameLbl);
 
-    firstNameArea = new JTextField();
-    firstNameArea.setForeground(Color.WHITE);
-    firstNameArea.setFont(new Font("Tahoma", Font.PLAIN, 13));
-    firstNameArea.setColumns(10);
-    firstNameArea.setBackground(new Color(102, 102, 102));
-    firstNameArea.setBounds(142, 64, 168, 20);
-    regPane.add(firstNameArea);
+		firstNameArea = new JTextField();
+		firstNameArea.setForeground(Color.WHITE);
+		firstNameArea.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		firstNameArea.setColumns(10);
+		firstNameArea.setBackground(new Color(102, 102, 102));
+		firstNameArea.setBounds(142, 64, 168, 20);
+		regPane.add(firstNameArea);
 
-    JLabel Minimise_1 = new JLabel("-");
-    Minimise_1.addMouseListener(new MouseAdapter() {
-      @Override
-      public void mouseClicked(MouseEvent e) {
-        setState(JFrame.ICONIFIED);
-      }
-    });
-    Minimise_1.setHorizontalAlignment(SwingConstants.CENTER);
-    Minimise_1.setForeground(Color.WHITE);
-    Minimise_1.setFont(new Font("Arial", Font.BOLD, 28));
-    Minimise_1.setBounds(275, 0, 27, 47);
-    regPane.add(Minimise_1);
+		JLabel Minimise_1 = new JLabel("-");
+		Minimise_1.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				setState(JFrame.ICONIFIED);
+			}
+		});
+		Minimise_1.setHorizontalAlignment(SwingConstants.CENTER);
+		Minimise_1.setForeground(Color.WHITE);
+		Minimise_1.setFont(new Font("Arial", Font.BOLD, 28));
+		Minimise_1.setBounds(275, 0, 27, 47);
+		regPane.add(Minimise_1);
 
-    JLabel Exit = new JLabel("X");
-    Exit.addMouseListener(new MouseAdapter() {
-      @Override
-      public void mouseClicked(MouseEvent e) {
-        System.exit(0);
-      }
-    });
-    Exit.setHorizontalAlignment(SwingConstants.CENTER);
-    Exit.setForeground(Color.WHITE);
-    Exit.setFont(new Font("Arial", Font.BOLD, 17));
-    Exit.setBounds(307, 0, 33, 52);
-    regPane.add(Exit);
+		JLabel Exit = new JLabel("X");
+		Exit.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				System.exit(0);
+			}
+		});
+		Exit.setHorizontalAlignment(SwingConstants.CENTER);
+		Exit.setForeground(Color.WHITE);
+		Exit.setFont(new Font("Arial", Font.BOLD, 17));
+		Exit.setBounds(307, 0, 33, 52);
+		regPane.add(Exit);
 
-    JLabel lastNameLbl = new JLabel("First Name   ");
-    lastNameLbl.setForeground(Color.WHITE);
-    lastNameLbl.setFont(new Font("Tahoma", Font.PLAIN, 12));
-    lastNameLbl.setBounds(26, 63, 109, 23);
-    regPane.add(lastNameLbl);
+		JLabel lastNameLbl = new JLabel("First Name   ");
+		lastNameLbl.setForeground(Color.WHITE);
+		lastNameLbl.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		lastNameLbl.setBounds(26, 63, 109, 23);
+		regPane.add(lastNameLbl);
 
-    lastNameArea = new JTextField();
-    lastNameArea.setForeground(Color.WHITE);
-    lastNameArea.setFont(new Font("Tahoma", Font.PLAIN, 13));
-    lastNameArea.setColumns(10);
-    lastNameArea.setBackground(new Color(102, 102, 102));
-    lastNameArea.setBounds(142, 94, 168, 20);
-    regPane.add(lastNameArea);
+		lastNameArea = new JTextField();
+		lastNameArea.setForeground(Color.WHITE);
+		lastNameArea.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		lastNameArea.setColumns(10);
+		lastNameArea.setBackground(new Color(102, 102, 102));
+		lastNameArea.setBounds(142, 94, 168, 20);
+		regPane.add(lastNameArea);
 
-    JLabel userNameLbl = new JLabel("Username   ");
-    userNameLbl.setForeground(Color.WHITE);
-    userNameLbl.setFont(new Font("Tahoma", Font.PLAIN, 12));
-    userNameLbl.setBounds(26, 123, 109, 23);
-    regPane.add(userNameLbl);
+		JLabel userNameLbl = new JLabel("Username   ");
+		userNameLbl.setForeground(Color.WHITE);
+		userNameLbl.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		userNameLbl.setBounds(26, 123, 109, 23);
+		regPane.add(userNameLbl);
 
-    userNameArea = new JTextField();
-    userNameArea.setForeground(Color.WHITE);
-    userNameArea.setFont(new Font("Tahoma", Font.PLAIN, 13));
-    userNameArea.setColumns(10);
-    userNameArea.setBackground(new Color(102, 102, 102));
-    userNameArea.setBounds(142, 124, 168, 20);
-    regPane.add(userNameArea);
+		userNameArea = new JTextField();
+		userNameArea.setForeground(Color.WHITE);
+		userNameArea.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		userNameArea.setColumns(10);
+		userNameArea.setBackground(new Color(102, 102, 102));
+		userNameArea.setBounds(142, 124, 168, 20);
+		regPane.add(userNameArea);
 
-    JLabel passwordLbl = new JLabel("Password             ");
-    passwordLbl.setForeground(Color.WHITE);
-    passwordLbl.setFont(new Font("Tahoma", Font.PLAIN, 12));
-    passwordLbl.setBounds(26, 153, 109, 23);
-    regPane.add(passwordLbl);
+		JLabel passwordLbl = new JLabel("Password             ");
+		passwordLbl.setForeground(Color.WHITE);
+		passwordLbl.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		passwordLbl.setBounds(26, 153, 109, 23);
+		regPane.add(passwordLbl);
 
-    JLabel confirmationLbl = new JLabel("Confirm Password ");
-    confirmationLbl.setForeground(Color.WHITE);
-    confirmationLbl.setFont(new Font("Tahoma", Font.PLAIN, 12));
-    confirmationLbl.setBounds(26, 188, 109, 23);
-    regPane.add(confirmationLbl);
+		JLabel confirmationLbl = new JLabel("Confirm Password ");
+		confirmationLbl.setForeground(Color.WHITE);
+		confirmationLbl.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		confirmationLbl.setBounds(26, 188, 109, 23);
+		regPane.add(confirmationLbl);
 
-    passwordArea = new JPasswordField();
-    passwordArea.setForeground(Color.WHITE);
-    passwordArea.setFont(new Font("Tahoma", Font.PLAIN, 13));
-    passwordArea.setBackground(new Color(102, 102, 102));
-    passwordArea.setBounds(142, 156, 168, 20);
-    regPane.add(passwordArea);
+		passwordArea = new JPasswordField();
+		passwordArea.setForeground(Color.WHITE);
+		passwordArea.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		passwordArea.setBackground(new Color(102, 102, 102));
+		passwordArea.setBounds(142, 156, 168, 20);
+		regPane.add(passwordArea);
 
-    confirmationArea = new JPasswordField();
-    confirmationArea.setForeground(Color.WHITE);
-    confirmationArea.setFont(new Font("Tahoma", Font.PLAIN, 13));
-    confirmationArea.setBackground(new Color(102, 102, 102));
-    confirmationArea.setBounds(142, 186, 168, 20);
-    regPane.add(confirmationArea);
+		confirmationArea = new JPasswordField();
+		confirmationArea.setForeground(Color.WHITE);
+		confirmationArea.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		confirmationArea.setBackground(new Color(102, 102, 102));
+		confirmationArea.setBounds(142, 186, 168, 20);
+		regPane.add(confirmationArea);
 
-    JButton btnConfirm = new JButton("Confirm");
-    btnConfirm.addMouseListener(new MouseAdapter() {
-      @Override
-      public void mouseClicked(MouseEvent e) {
-        if (getPass().equals(getConfirmPass())) {
-          try {
-            if (remoteGame.isUsernameExisted(userNameArea.getText())) {
-              // Error when username has already existed
-              JOptionPane.showMessageDialog(null, "Username exists. Please create another username",
-                  "Error", JOptionPane.PLAIN_MESSAGE);
-            } else {
-              // Successful registering
-              remoteGame.appendJsonClient(getUserName(), getPass(), getFirstName(), getLastName());
-            }
-          } catch (RemoteException e1) {
-            e1.printStackTrace();
-          }
+		JButton btnConfirm = new JButton("Confirm");
+		btnConfirm.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				if (getPass().equals(getConfirmPass())) {
+					try {
+						if (remoteGame.isUsernameExisted(userNameArea.getText())) {
+							// Error when username has already existed
+							JOptionPane.showMessageDialog(null, "Username exists. Please create another username",
+									"Error", JOptionPane.PLAIN_MESSAGE);
+						} else {
+							// Successful registering
+							dispose();
+							JOptionPane.showMessageDialog(null, "Registration succeful.", "Success",
+									JOptionPane.PLAIN_MESSAGE);
+							Login login = new Login(remoteGame);
+							login.setVisible(true);
+							login.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+							remoteGame.appendJsonClient(getUserName(), getPass(), getFirstName(), getLastName());
+						}
+					} catch (RemoteException e1) {
+						e1.printStackTrace();
+					}
 
-        } else {
-          // Error when confirmation doesn't match password
-          JOptionPane.showMessageDialog(null,
-              "Confirmation does not match password. Please retype again", "Error",
-              JOptionPane.PLAIN_MESSAGE);
-        }
+				} else {
+					// Error when confirmation doesn't match password
+					JOptionPane.showMessageDialog(null, "Confirmation does not match password. Please retype again",
+							"Error", JOptionPane.PLAIN_MESSAGE);
+				}
 
-      }
-    });
-    btnConfirm.setFont(new Font("Tahoma", Font.PLAIN, 13));
-    btnConfirm.setBackground(Color.WHITE);
-    btnConfirm.setBounds(71, 223, 90, 29);
-    regPane.add(btnConfirm);
+			}
+		});
+		btnConfirm.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		btnConfirm.setBackground(Color.WHITE);
+		btnConfirm.setBounds(71, 223, 90, 29);
+		regPane.add(btnConfirm);
 
-    JButton button_1 = new JButton("Cancel");
-    button_1.setFont(new Font("Tahoma", Font.PLAIN, 13));
-    button_1.setBounds(178, 223, 90, 29);
-    regPane.add(button_1);
+		JButton button_1 = new JButton("Cancel");
+		button_1.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				dispose();
+				Login login = new Login(remoteGame);
+				login.setVisible(true);
+				login.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			}
+		});
+		button_1.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		button_1.setBounds(178, 223, 90, 29);
+		regPane.add(button_1);
 
-    setUndecorated(true);
-  }
+		setUndecorated(true);
+	}
 
-  public String getFirstName() {
-    String name = firstNameArea.getText();
-    return name;
-  }
+	public String getFirstName() {
+		String name = firstNameArea.getText();
+		return name;
+	}
 
-  public String getLastName() {
-    String name = lastNameArea.getText();
-    return name;
-  }
+	public String getLastName() {
+		String name = lastNameArea.getText();
+		return name;
+	}
 
-  public String getUserName() {
-    String name = userNameArea.getText();
-    return name;
-  }
+	public String getUserName() {
+		String name = userNameArea.getText();
+		return name;
+	}
 
-  public String getPass() {
-    String name = new String(passwordArea.getPassword());
-    return name;
-  }
+	public String getPass() {
+		String name = new String(passwordArea.getPassword());
+		return name;
+	}
 
-  public String getConfirmPass() {
-    String name = new String(confirmationArea.getPassword());
-    return name;
-  }
+	public String getConfirmPass() {
+		String name = new String(confirmationArea.getPassword());
+		return name;
+	}
 }
